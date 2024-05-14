65c3e4402eec2cc312c2409f15a9d5392bcabc95
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 11:19:38 -0400

    funcoes/declaracao-funcao-com-parametros.js

---
+++ funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro1, parametro2) {
+    //corpo da funcao
+    //pode contar uma ou mais instrucoes
+    return resultado; // opicional
+}
+// chamada de funcao
+nomeDaFuncao(parametro1, parametro2);
