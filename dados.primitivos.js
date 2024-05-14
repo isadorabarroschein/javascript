47585b97a3d7b4756544a6f62d4db5ee8dd362c0
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 09:52:48 -0400

    adicionado exemplo de dados primitivos

---
+++ dados.primitivos.js
@@ -0,0 +1,13 @@
+let nome = "isadora"; // Tipo String 
+let verdadeiro = true; // Tipo Boolean 
+let falso = false; // Tipo Boolean
+let numero = 17; //Tipo Number
+let reais = 25.99; // Tipo Number
+let nulo = null; // Tipo Null
+let indefinido; //Tipo UNderfined
+let NotANumber = 0 / 0; //Tipo NaN
+let objetivo = {  // Tipo Object
+    nome: "isadora",
+    age :17,
+    Profession: estudante 
+}
