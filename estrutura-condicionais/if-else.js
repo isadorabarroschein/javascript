686cfeab1798e9284e9840373229108e51d90091
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 09:55:03 -0400

    adicionado exemplo de estrutura-condicionais if-else

---
+++ estrutura-condicionais/if-else.js
@@ -0,0 +1,7 @@
+let diaDaSemana = "quarta-feira";
+
+if(diaDaSemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("E fim de semana!");
+} else {
+    console.log("É dia da semana.");
+}
