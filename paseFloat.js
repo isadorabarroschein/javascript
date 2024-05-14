13737814c48067f5c09758661e3f5813190599e2
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 10:05:37 -0400

    adicionado exemplo paseFloat

---
+++ paseFloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.68";
+console.log(stringParaDecimal); //Saída "12.68"
+console.log(typeof(stringParaDecimal)); // Saída: string
+console.log(""); // Saída "12.68"
+//Conversão de string "stringParaDecimal" para Decimal
+let decimal = parseFloat(stringParaDecimal);
+console.log(decimal); //Saída 12.68
+console.log(typeof(decimal)); //Saída: number
