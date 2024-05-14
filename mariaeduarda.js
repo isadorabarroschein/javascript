288c6475ee3d4345bada45e43fa55164d8934051
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 10:03:33 -0400

    adicionado exemplo de mariaeduarda

---
+++ mariaeduarda.js
@@ -0,0 +1,17 @@
+let textoMultiplaLinha = `
+Esta é uma string
+que ocupa multiplas linhas  
+e você não precisa se preocupar 
+com quebras de linhas ou concatenação
+`;
+console.log(textoMultiplaLinha);
+/*
+Saída:
+" 
+esta é uma string 
+que ocupa multiplas linhas
+e você não precisa se procupar 
+com quebras de linhas ou concatenação
+" 
+*/ 
+
