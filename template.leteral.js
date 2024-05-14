c78371f617dcdb92c8c3db998e85bd1d29a79cf4
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 10:05:00 -0400

    adicionado exemplo de lateral

---
+++ template.leteral.js
@@ -0,0 +1,7 @@
+let nome = "Isadora Chein";
+let idade = 17;
+
+//
+// Usando template literals para criar um string com variaveis
+let saudacao = `Ola, meu nome e ${nome} e eu tenho ${idade} anos.`; 
+console.log(saudacao); // Saida: "Ola, meu nome e Isadora Chein e eu tenho 17 anos."
