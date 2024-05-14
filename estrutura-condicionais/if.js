e2844cfb4bcff4e577a5fd1aaf950bb91d9674d4
isadorabarroschein <isadorabc437@gmail.com>
2024-05-14 09:44:52 -0400

    adicionado estrutura condicional if

---
+++ estrutura-condicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) { 
+    console.groupCollapsed("Bom dia");
+}
+// o restante do codigo continua independente se a condição for verdadeira ou não 
+console.log("Dinheiro nao traz felicidade. Me de o seu e seja feliz!");
